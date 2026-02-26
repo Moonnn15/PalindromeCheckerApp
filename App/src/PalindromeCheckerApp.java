d88227fab2d5cd891145370c8c3f24813ce36b14
Moonnn15 <monabhtrig@gmail.com>
2026-02-27 00:09:26 +0530

    Monalisa Bhattacharjee Added UC3

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,22 +2,9 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String word = "madam";
-
-        String reversed = new StringBuilder(word).reverse().toString();
-
-        if (word.equals(reversed)) {
-            System.out.println("The given string \"" + word + "\" is a Palindrome.");
-        } else {
-            System.out.println("The given string \"" + word + "\" is NOT a Palindrome.");
-        }
-        System.out.println("Program ended.");
-
-
-        // 3 rd program
         String original = "noon";
 
-
+        String reversed = "";
         for (int i = original.length() - 1; i >= 0; i--) {
             reversed = reversed + original.charAt(i);
         }
